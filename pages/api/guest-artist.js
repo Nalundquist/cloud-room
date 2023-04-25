0ce65f4d1b378d62906d63773cb04b7b7cc38aa7
Noah Lundquist <nalundquist@gmail.com>
2023-04-25 15:58:04 -0700

    complete guestartist nodeJS backend

--- pages/api/guest-artist.js
+++ pages/api/guest-artist.js
@@ -1,10 +1,13 @@
+import fs from 'fs';
+import path from 'path';
 
+let path = require("path");
+let http = require("http");
 
 function handler(req, res) {
 	if (req.method == "POST") {
 
 		const artistRequest = req.body.artistRequest;
-
 		const newArtistRequest = {
 			id: new Date().toISOString(),
 		  name: artistRequest.nameInputRef,
@@ -17,8 +20,16 @@ function handler(req, res) {
 			message: artistRequest.message,
 			accepted: artistRequest.accepted
 		}
+		const filePath = path.join(process.cwd(), 'data', 'guestArtist.json');
+		const fileData = fs.readFileSync(filePath);
+		const data = JSON.parse(fileData);
+		data.push(newArtistRequest);
+		fs.writeFileSync(filePath, JSON.stringify(data));
+		res.status(201).json({message: "successful upload", artistRequest: newArtistRequest});
+
+	} else {
+		res.status(200).json({message: "functioning"}); 
 	}
-	res.status(200).json({message: "functioning"}); 
 }
 
 export default handler;
